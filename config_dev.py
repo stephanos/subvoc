a0ab36816c214aa9f637f39c4f3c1855c81fb321
stephanos <github@stephanbehnke.de>
2016-12-20 12:42:48 +0100

    integrate API credentials into config

--- config_dev.py
+++ config_dev.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import credentials
+
 # flask core settings
 DEBUG = True
 TESTING = False
@@ -7,3 +9,5 @@ SECRET_KEY = 'qh\x98\xc4o\xc4]\x8f\x8d\x93\xa4\xec\xc5\xfd]\xf8\xb1c\x84\x86\xa7
 PERMANENT_SESSION_LIFETIME = 60 * 60 * 24 * 30
 
 # project settings
+OPENSUBTITLES_USER = credentials.OPENSUBTITLES_USER
+OPENSUBTITLES_PASS = credentials.OPENSUBTITLES_PASS
