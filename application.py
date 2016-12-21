7b9d97f25a415dd4df2201afc2130c4fabc584eb
stephanos <github@stephanbehnke.de>
2016-12-21 14:35:08 +0100

    create landing page

--- application.py
+++ application.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 
 def create_app(config_filename):
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object(config_filename)
     return app
--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
 from application import create_app
+from flask import render_template
+
+
 app = create_app('config_dev')
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
 app.run()
