6e82a15a1e6475b1b25627f659e506a93d9fb298
stephanos <github@stephanbehnke.de>
2016-12-21 15:40:08 +0100

    fix: use dev config locally

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template
 
 
-config_filename = 'config_' + (os.environ.get('env', 'prod')).lower()
+config_filename = os.environ.get('CONFIG', 'config_dev')
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -12,4 +12,3 @@ app.config.from_object(config_filename)
 @app.route('/')
 def home():
     return render_template('home.html')
-
