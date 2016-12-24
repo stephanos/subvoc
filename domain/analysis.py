dbc0cda12ac0a6f6363b30fa85b66609a6934f0c
stephanos <github@stephanbehnke.de>
2016-12-24 11:05:58 +0100

    emit list of words by frequency

--- domain/analysis.py
+++ domain/analysis.py
@@ -1,3 +1,23 @@
+import collections, nltk, re
+from sortedcontainers import SortedSet, SortedDict
+from nltk import pos_tag
+from nltk.corpus import wordnet
+from nltk.tokenize import WordPunctTokenizer
+
+
+WordFreq = collections.namedtuple('WordFreq', 'word freq')
+
+
+def load_corpus(filepath):
+    words_by_freq = SortedDict()
+    with open(filepath, 'r') as file:
+        for line in file:
+            word, freq = line.split(' ')
+            words_by_freq[word] = int(freq)
+    return words_by_freq
+
+corpus = load_corpus('corpus/en.txt')
+
 def pick_best(subtitles):
     single_subtitles = [s for s in subtitles if s['SubSumCD'] == '1']
     if not single_subtitles:
@@ -27,8 +47,20 @@ def load_subtitle(api, imdb_id):
 
     return subtitle_text
 
+def analyse_subtitles(subtitles):
+    # TODO transform into tuple (start, end, text)
+    word_by_freq = SortedSet(key = lambda x: x.freq)
+    tokenizer = WordPunctTokenizer()
+    tokens = tokenizer.tokenize(subtitles)
+    for token in tokens:
+        word = token # todo use lemma
+        if word in corpus:
+            freq = corpus[word]
+            word_by_freq.add(WordFreq(word, freq))
+    return list(word_by_freq)
+
 
 def analyse_movie(api, imdb_id):
     subtitle_text = load_subtitle(api, imdb_id)
-    # TODO
-
+    analysis = analyse_subtitles(subtitle_text)
+    return analysis
---
+++ domain/analysis_test.py
@@ -0,0 +1,19 @@
+from analysis import analyse_subtitles, WordFreq
+
+def test_sort():
+    assert analyse_subtitles(
+'''
+83
+00:06:49,765 --> 00:06:53,060
+I can honestly say
+that I'm a changed man.
+''') == [
+    WordFreq(word='honestly',   freq=23429),
+    WordFreq(word='m',          freq=43966),
+    WordFreq(word='changed',    freq=67059),
+    WordFreq(word='say',        freq=901201),
+    WordFreq(word='man',        freq=910369),
+    WordFreq(word='can',        freq=2974080),
+    WordFreq(word='that',       freq=7789269),
+    WordFreq(word='a',          freq=11230036)
+]
