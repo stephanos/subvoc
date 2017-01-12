4cd63fc8e7c7e88e33786302c77079ddcecec3c0
stephanos <github@stephanbehnke.de>
2017-01-12 08:23:20 +0100

    use Flask-Assets for bundling stylesheets

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import os
 from flask import Flask, render_template, request
+from flask_assets import Bundle, Environment
 
 from domain.search import search
 from domain.analysis import analyse
 from api.subtitle.opensubtitles import OpenSubtitles
 
 
-config_filename = os.environ.get('CONFIG', 'config_dev')
+CSS_ROOT = 'static/css'
+
+
 app = Flask(__name__, static_url_path='/static')
+assets = Environment(app)
 
-
+config_filename = os.environ.get('CONFIG', 'config_dev')
 app.config.from_object(config_filename)
+
+for directory in os.listdir(CSS_ROOT):
+    dir_path = os.path.join(os.getcwd(), CSS_ROOT, directory)
+    if not os.path.isdir(dir_path):
+        continue
+    asset_paths = [os.path.join(dir_path, f) for f in os.listdir(dir_path)]
+    b = Bundle(*asset_paths, filters='cssmin', output='generated/%s.css' % directory)
+    assets.register(directory, b)
+
 OPENSUBTITLES_CREDENTIALS = (app.config['OPENSUBTITLES_USER'], app.config['OPENSUBTITLES_PASS'])
 
 
