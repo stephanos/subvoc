afce252cec0e20d0ce1e475b0a9ac3d60cea5317
stephanos <github@stephanbehnke.de>
2017-01-27 16:26:00 +0100

    remove print statement

--- api/fixture.py
+++ api/fixture.py
@@ -9,7 +9,6 @@ def load_fixture(template, arg, parse_fn=None):
     if arg.isalnum():  # prevent reading other files
         file_path = os.path.join(FIXTURE_DIR, template.format(arg))
         with suppress(OSError), open(file_path) as file:
-            print(file_path)
             data = file.read()
             if parse_fn:
                 return parse_fn(data)
