2bd7f294697644a2fce54a7a919204b5f94e583c
Stephan Behnke <git@stephanbehnke.de>
2017-04-20 08:22:57 -0400

    fix: adapt to new enum names

--- domain/lemmatize_test.py
+++ domain/lemmatize_test.py
@@ -5,7 +5,7 @@ from domain.tagger import PartOfSpeach
 def test_lemmatize_inflected():
     lemmatizer = Lemmatizer()
 
-    assert lemmatizer.lemmatize('bluest', PartOfSpeach.ADJ) == 'blue'
+    assert lemmatizer.lemmatize('bluest', PartOfSpeach.ADJECTIVE) == 'blue'
     assert lemmatizer.lemmatize('friends', PartOfSpeach.NOUN) == 'friend'
     assert lemmatizer.lemmatize('being', PartOfSpeach.VERB) == 'be'
 
@@ -13,8 +13,8 @@ def test_lemmatize_inflected():
 def test_do_not_lemmatize_uninflected():
     lemmatizer = Lemmatizer()
 
-    assert lemmatizer.lemmatize('blue', PartOfSpeach.ADJ) == 'blue'
-    assert lemmatizer.lemmatize('lightly', PartOfSpeach.ADV) == 'lightly'
+    assert lemmatizer.lemmatize('blue', PartOfSpeach.ADJECTIVE) == 'blue'
+    assert lemmatizer.lemmatize('lightly', PartOfSpeach.ADVERB) == 'lightly'
     assert lemmatizer.lemmatize('friend', PartOfSpeach.NOUN) == 'friend'
     assert lemmatizer.lemmatize('hope', PartOfSpeach.VERB) == 'hope'
     assert lemmatizer.lemmatize('and', PartOfSpeach.OTHER) == 'and'
